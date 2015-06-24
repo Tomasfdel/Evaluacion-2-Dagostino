2043444bcdddcbfa859eacd3cc6be104b70e9997
Tomasfdel <Tomasfdel@users.noreply.github.com>
2015-06-24 17:46:34 -0300

    Corregimos satisfactoriamente el funcionaminto

--- prueba.py
+++ prueba.py
@@ -254,27 +254,27 @@ class Piramide:
 			
 			
 	def set_valores(self):
-		#self.celda00.set_valor(0)
+		self.celda00.set_valor(58)
 		#self.celda10.set_valor(0)
 		#self.celda11.set_valor(0)
 		#self.celda20.set_valor(0)
 		#self.celda21.set_valor(0)
 		#self.celda22.set_valor(0)
 		#self.celda30.set_valor(0)
-		#self.celda31.set_valor(0)
+		self.celda31.set_valor(6)
 		#self.celda32.set_valor(0)
-		#self.celda33.set_valor(0)
-		#self.celda40.set_valor(0)
+		self.celda33.set_valor(10)
+		self.celda40.set_valor(7)
 		#self.celda41.set_valor(0)
 		#self.celda42.set_valor(0)
 		#self.celda43.set_valor(0)
-		#self.celda44.set_valor(0)
-		#self.celda50.set_valor(0)
-		#self.celda51.set_valor(0)
-		#self.celda52.set_valor(0)
-		#self.celda53.set_valor(0)
-		#self.celda54.set_valor(0)
-		#self.celda55.set_valor(0)
+		self.celda44.set_valor(5)
+		#self.celda50.set_valor(2)
+		#self.celda51.set_valor(3)
+		self.celda52.set_valor(0)
+		self.celda53.set_valor(945)
+		#self.celda54.set_valor(3)
+		#self.celda55.set_valor()
 			
 	
 			
